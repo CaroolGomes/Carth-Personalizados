a589bf366e6f92d3bee29bcb7ba8e4f5c0ff142e
CaroolGomes <137942117+CaroolGomes@users.noreply.github.com>
2026-02-04 18:01:20 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,17 +1,51 @@
-const params = new URLSearchParams(window.location.search);
-const cat = params.get("cat");
+async function carregarDados() {
+const resp = await fetch("data/produtos.json")
+return resp.json()
+}
 
-fetch("data/produtos.json")
-.then(r=>r.json())
-.then(data=>{
-  document.getElementById("titulo").innerText = cat.toUpperCase();
-  data[cat].forEach(p=>{
-    document.getElementById("lista").innerHTML += `
-      <div class="card">
-        <h3>${p.nome}</h3>
-        <p>${p.descricao}</p>
-        <a href="produto.html?cat=${cat}&id=${p.id}">Ver produto</a>
-      </div>
-    `;
-  });
-});
+const urlParams = new URLSearchParams(window.location.search)
+const categoriaID = urlParams.get("categoria")
+
+carregarDados().then(data => {
+
+if(document.getElementById("categorias")){
+
+let html = ""
+
+data.categorias.forEach(cat=>{
+html += `
+<div class="card">
+<h3>${cat.nome}</h3>
+<a href="categoria.html?categoria=${cat.id}">
+<button>Ver</button>
+</a>
+</div>
+`
+})
+
+document.getElementById("categorias").innerHTML = html
+}
+
+if(categoriaID){
+
+const categoria = data.categorias.find(c=>c.id==categoriaID)
+
+document.getElementById("tituloCategoria").innerText = categoria.nome
+
+let html = ""
+
+categoria.produtos.forEach(prod=>{
+html += `
+<div class="card">
+<h3>${prod.nome}</h3>
+<a href="produto.html?categoria=${categoria.id}&produto=${prod.id}">
+<button>Ver Produto</button>
+</a>
+</div>
+`
+})
+
+document.getElementById("produtos").innerHTML = html
+}
+
+})
