15de4c0603aed076288161ea342e58a7c426fabe
CaroolGomes <137942117+CaroolGomes@users.noreply.github.com>
2026-02-04 22:20:47 -0300

    Update calculos.js

--- js/calculos.js
+++ js/calculos.js
@@ -1,3 +1,17 @@
 function calcularM2(h, w, preco) {
   return h * w * preco;
 }
+export function calcularTotal(carrinho){
+
+    let total = 0;
+
+    carrinho.forEach(item => {
+
+        if(item.preco){
+            total += item.preco * item.quantidade;
+        }
+
+    });
+
+    return total;
+}
